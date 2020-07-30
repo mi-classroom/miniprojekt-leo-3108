1482137d7a64313c6ddc3dd24207cc6fde29087b
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-07-30 11:01:23 +0200

    Header ist jetzt fixiert

---
+++ dist/scripts/pictureblock.js
@@ -0,0 +1,45 @@
+async function fetchData(apiURL, parseJSON = true) {
+  const response = await fetch(apiURL);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  let data = null;
+  if (parseJSON) {
+    data = await response.json();
+  } else {
+    data = await response.text();
+  }
+  return data;
+}
+
+async function main() {
+  let DataDe = await fetchData('./data/json/cda-paintings-v2.de.json');
+  DataDe = DataDe.items;
+  let DataEn = await fetchData('./data/json/cda-paintings-v2.en.json');
+  DataEn = DataEn.items;
+
+  let year = [];
+
+  DataDe.forEach((bild) => {
+    const tmp = bild.dating.begin;
+    year.push(tmp);
+  });
+
+  year.sort();
+  year = year.filter((elem, index, self) => index === self.indexOf(elem));
+
+  for (let i = 0; i < year.length; i++) {
+    year[i] = { year: year[i] };
+  }
+
+  console.log(year);
+
+  const mustacheElement = document.querySelector('main');
+
+  const yearTemplate = await fetchData('./templates/pictureblock.html', false);
+
+  const renderedSection = Mustache.render(yearTemplate, { year });
+  mustacheElement.innerHTML = renderedSection;
+}
+
+main();
